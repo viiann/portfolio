fefa3f64d2881afc7571e7a504184c5cfd8d0ea6
viiann <vianney.thllz@gmail.com>
2019-07-08 15:39:39 +0200

    first commit

---
+++ competences/script.js
@@ -0,0 +1,20 @@
+/*****************************************************************************************************
+ *                              FONCTION POUR FAIRE APPARAITRE MES TEXTES
+ * 
+ ******************************************************************************************************/
+/*(function(){
+    var button = document.querySelectorAll(".click");
+
+    //foreach = pour chaque carre du tableau button    => je fait ça
+    //autre syntaxe //button.forEach(function(carre){});
+    
+    
+ /*   button.forEach(carre => {
+        carre.addEventListener("click",function(){
+            console.log("click");
+            var texte = carre.querySelector('div');
+            texte.classList.toggle('actif'); 
+    })
+    });
+})();
+
